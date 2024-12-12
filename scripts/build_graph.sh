6b3e92d40a78aec8495c233e9aff8fea12c36534
aya221 <82810579+aya221@users.noreply.github.com>
2024-12-12 17:51:21 +0100

    Update build_graph.sh

--- scripts/build_graph.sh
+++ scripts/build_graph.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export SOURCE_DIR=$PWD
+echo $SOURCE_DIR
 JENA_LOC=$1
 QUADS_LOC=$2
 JENA_DB_LOC=$3
@@ -8,7 +10,12 @@ cd $JENA_LOC
 #mkdir logs
 cd bin
 chmod +x ./tdb2.tdbloader
-export JVM_ARGS=-Xmx32G
+#export JVM_ARGS=-Xmx1T
+#echo $QUADS_LOC/*.nq.bz2 
+#echo $PWD
+#echo $(ls /pfss/mlde/workspaces/mlde_wsp_KIServiceCenter/ec38sifi/graph4code/scripts/graph4code_quads/*.nq.bz2)
+
+chmod +r $SOURCE_DIR/graph4code/scripts/graph4code_quads/*
 #./tdb2.tdbloader --loader=parallel --loc=$JENA_DB_LOC $QUADS_LOC/*.nq.bz2 > ../logs/load_graph_v1.log 2>&1 
-./tdb2.tdbloader --loader=parallel --loc=$JENA_DB_LOC $QUADS_LOC/*.nq.bz2 
+./tdb2.tdbloader --loader=parallel --loc=$JENA_DB_LOC $QUADS_LOC/*
 
