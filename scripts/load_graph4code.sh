97754dfc50ddecb300f1264c0d582809fb8ee4d8
aya221 <82810579+aya221@users.noreply.github.com>
2024-12-12 17:49:51 +0100

    Update load_graph4code.sh

--- scripts/load_graph4code.sh
+++ scripts/load_graph4code.sh
@@ -6,9 +6,9 @@
 #chmod +x *.sh
 
 echo "-------Downloading Graph4Code Files-----"
-export SOURCE_DIR=$PWD
-export QUADS_LOC=$SOURCE_DIR/graph4code_quads/
-export JENA_DB_LOC=$SOURCE_DIR/graph4code_db/
+export SOURCE_DIR=$PWD/graph4code/scripts
+export QUADS_LOC=$SOURCE_DIR/graph4code_quads
+export JENA_DB_LOC=$SOURCE_DIR/graph4code_db
 export JENA_LOC=$SOURCE_DIR/apache-jena-3.16.0
 export FUSEKI_LOC=$SOURCE_DIR/apache-jena-fuseki-3.16.0
 echo "SOURCE_DIR: $SOURCE_DIR"
@@ -17,29 +17,27 @@ echo "QUADS_LOC: $QUADS_LOC"
 echo "JENA_DB_LOC: $JENA_DB_LOC"
 #-------Setup----------#
 echo "Main Directory: $source_dir"
-git clone https://github.com/wala/graph4code.git
+#git clone https://github.com/wala/graph4code.git
 mkdir $QUADS_LOC
 mkdir $JENA_DB_LOC
-wget http://mirror.metrocast.net/apache/jena/binaries/apache-jena-fuseki-3.16.0.tar.gz
+wget https://archive.apache.org/dist/jena/binaries/apache-jena-fuseki-3.16.0.tar.gz
 tar -xzf apache-jena-fuseki-3.16.0.tar.gz
 
-wget http://mirror.metrocast.net/apache/jena/binaries/apache-jena-3.16.0.tar.gz
+wget https://archive.apache.org/dist/jena/binaries/apache-jena-3.16.0.tar.gz
 tar -xzf apache-jena-3.16.0.tar.gz
 #-------END OF Setup----------#
 
-cd $SOURCE_DIR/graph4code/scripts
+cd $SOURCE_DIR
 chmod +x *.sh
-./download_graph.sh $QUADS_LOC
-
+#./download_graph.sh $QUADS_LOC
 
 echo "-------Build Graph4Code JENA DB-----"
-cd $SOURCE_DIR/graph4code/scripts
+cd $SOURCE_DIR
 ./build_graph.sh $JENA_LOC $QUADS_LOC $JENA_DB_LOC
 
 echo "-------Launch FUSEKI over Graph4Code JENA DB: log available at $FUSEKI_LOC/log-----"
-cd $SOURCE_DIR/graph4code/scripts
+cd $SOURCE_DIR
 ./serve_graph.sh $FUSEKI_LOC $JENA_DB_LOC
-
 
 echo "Testing sample query"
 cd $FUSEKI_LOC/bin/
